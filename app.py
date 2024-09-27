1e9b7a08fdd4db33f721a5534ab1d7ad242659fc
Saravanan G <saravanan.off2017@gmail.com>
2024-09-27 09:58:16 +0530

    Updated1

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
-import streamlit as st 
+# Build a Recommender System
+import streamlit as st
 from langchain import PromptTemplate, LLMChain
 from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 
-# Import the local environment
-from dotenv import load_dotenv # for api key storing
-import os
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
+# Import the Local Environment
+from dotenv import load_dotenv
+import os 
+genai.configure(api_key=os.getenv('GOOGLE-API-KEY'))
 
+#Design the Page...  streamlit run app.py
+headers={"authorization":st.secrets['GOOGLE-API-KEY'],"Content-type":"application/json"}
 
-# Design the Page ... streamlit run app.py
-header = {'authorization':st.secrets['GOOGLE-API-KEY'],
-          "content-type":"application/json"}
-st.title("Movie Recommender Systems")
-user_input = st.text_input("Enter the Movie Title,genre or keyword")
+#Design the Page...
+st.title("Generate Movie Recommendations...")
+user_input=st.text_input('Enter the Movie title,genre or keyword')
 
-# creating a prompt Template
-demo_template = ''' Give me movie recommendations for the following genre {prompt}'''
-template = PromptTemplate(
-    input_variables = ['prompt'],
-    template = demo_template)
-
+# Prompt Template
+demo_template='''Based on {user_input} provides 10 Movie Recommendation with one liner plot detail'''
+template=PromptTemplate(input_variable=['user_input'],template=demo_template)
 
 # Google Gemini Model
-llm = ChatGoogleGenerativeAI(model="gemini-pro",api_key=os.getenv("GOOGLE-API-KEY"))
-
+llm=ChatGoogleGenerativeAI(model='gemini-pro',api_key=os.getenv('GOOGLE-API-KEY'))
 
 if user_input:
-    prompt = template.format(user_input=user_input)
-    recommendations = llm.predict(text=prompt)
-    st.write(f"Recommendations: for you :\n {recommendations}")
+    prompt=template.format(user_input=user_input)
+    recommendations=llm.predict(text=prompt)
+    st.write(f'Recommendations for you : \n {recommendations}')
 else:
-    st.write(' ')
-
-
-
-
-
+    st.write('Please enter the Movie Name')
